0083219d6a077dfe77d6440bb789b4eedb184b61
navdeepbedi <navddep.singh@appster.in>
2018-03-30 11:16:55 +0530

    init

---
+++ app/src/main/java/com/droid/nav/cp/MainActivity.java
@@ -0,0 +1,69 @@
+package com.droid.nav.cp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+
+    private static final String TAG=MainActivity.class.getName();
+
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+
+        } else {
+            Log.d(TAG, "OpenCV loaded");
+            System.loadLibrary("native-lib");
+        }
+    }
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.fd_activity_surface_view);
+        mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(MainActivity.this);
+        mOpenCvCameraView.enableView();
+
+
+    }
+
+
+
+    public native void salt(long matAddrGray, int nbrElem);
+
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat matGray = inputFrame.gray();
+        salt(matGray.getNativeObjAddr(), 2000);
+        return matGray;
+    }
+
+
+}
