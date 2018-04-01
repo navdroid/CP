99abc36c10cda758a5d991677cd38b9a34a07080
navdeepbedi <navddep.singh@appster.in>
2018-04-01 14:09:38 +0530

    sunflower detection added

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,6 +3,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 
 using namespace std;
 using namespace cv;
@@ -18,5 +20,27 @@ extern "C"
             int j = rand() % mGr.rows;
             mGr.at<uchar>(j, i) = 255;
         }
+    }
+
+    void JNICALL Java_com_droid_nav_cp_MainActivity_chr(JNIEnv *env, jobject instance, jlong matAddrGray) {
+
+        Mat &img = *(Mat *) matAddrGray;
+        Ptr<MSER> ms = MSER::create();
+        vector<vector<Point> > regions;
+        vector<cv::Rect> mser_bbox;
+        ms->detectRegions(img, regions, mser_bbox);
+
+        for (int i = 0; i < regions.size(); i++)
+        {
+            rectangle(img, mser_bbox[i], CV_RGB(0, 255, 0));
+        }
+
+    }
+
+    void JNICALL Java_com_droid_nav_cp_MainActivity_hr(JNIEnv *env, jobject instance, jlong matAddrGray) {
+
+
+
+
     }
 }
--- app/src/main/java/com/droid/nav/cp/MainActivity.java
+++ app/src/main/java/com/droid/nav/cp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.droid.nav.cp;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,24 +14,42 @@ import android.widget.Toast;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-    private static final String TAG=MainActivity.class.getName();
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG = MainActivity.class.getName();
+    private static final org.opencv.core.Scalar FACE_RECT_COLOR = new Scalar(0, 255, 0, 255);;
 
     static {
-        if(!OpenCVLoader.initDebug()){
+        if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "OpenCV not loaded");
 
         } else {
             Log.d(TAG, "OpenCV loaded");
             System.loadLibrary("native-lib");
 
-
         }
     }
 
     private CameraBridgeViewBase mOpenCvCameraView;
+    private File mCascadeFile;
+    private CascadeClassifier mJavaDetector;
+    private DetectionBasedTracker mNativeDetector;
+    private Mat mGray;
+    private Mat mRgba;
+    private int mAbsoluteFaceSize=0;
+    private float mRelativeFaceSize= 0.2f;;
 
 
     @Override
@@ -50,7 +69,43 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         mOpenCvCameraView.setCvCameraViewListener(MainActivity.this);
 
 
+    }
 
+
+    private void initCamera() {
+        try {
+            // load cascade file from application resources
+            //Change  <sunflower_detection> to  <eyes_detection> here to check eyes detection algorithm -
+            InputStream is = getResources().openRawResource(R.raw.sunflower_detection);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            mCascadeFile = new File(cascadeDir, "sunflower_detection.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+            mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            if (mJavaDetector.empty()) {
+                Log.e(TAG, "Failed to load cascade classifier");
+                mJavaDetector = null;
+            } else
+                Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+
+            mNativeDetector = new DetectionBasedTracker(mCascadeFile.getAbsolutePath(), 0);
+
+            cascadeDir.delete();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+        }
+
+        mOpenCvCameraView.enableView();
     }
 
     @Override
@@ -93,27 +148,57 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     protected void onResume() {
         super.onResume();
-        mOpenCvCameraView.enableView();
+        initCamera();
     }
 
-    public native void salt(long matAddrGray, int nbrElem);
+
 
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-
+        mGray = new Mat();
+        mRgba = new Mat();
     }
 
     @Override
     public void onCameraViewStopped() {
-
+        mGray.release();
+        mRgba.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Mat matGray = inputFrame.gray();
-        salt(matGray.getNativeObjAddr(), 2000);
-        return matGray;
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+
+        if (mAbsoluteFaceSize == 0) {
+            int height = mGray.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+            mNativeDetector.setMinFaceSize(mAbsoluteFaceSize);
+        }
+
+        MatOfRect faces = new MatOfRect();
+
+//        if (mDetectorType == JAVA_DETECTOR) {
+//            if (mJavaDetector != null)
+//                mJavaDetector.detectMultiScale(mGray, faces, 1.1, 2, 2, // TODO: objdetect.CV_HAAR_SCALE_IMAGE
+//                        new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+//        }
+//        else if (mDetectorType == NATIVE_DETECTOR) {
+            if (mNativeDetector != null)
+                mNativeDetector.detect(mGray, faces);
+//        }
+//        else {
+//            Log.e(TAG, "Detection method is not selected!");
+//        }
+
+        Rect[] facesArray = faces.toArray();
+        for (int i = 0; i < facesArray.length; i++)
+            Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), FACE_RECT_COLOR, 3);
+
+        return mRgba;
     }
 
 
